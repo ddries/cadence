9f529a877ae59d63f4f18b6a4ca73caad8796668
ddries <driescode@gmail.com>
2023-02-06 02:11:55 +0100

    disable redis for build

--- src/Cadence.ts
+++ src/Cadence.ts
@@ -76,7 +76,7 @@ export default class Cadence {
 
         await CadenceMemory.getInstance().init();
         // await CadenceWebsockets.getInstance().init();
-        await CadenceRedis.getInstance().init();
+        // await CadenceRedis.getInstance().init();
 
         this.logger.log('starting discord module cadence ' + Cadence.Version + ", debug " + Cadence.Debug.toString());
         await CadenceDiscord.getInstance().init();
